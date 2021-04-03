47bd4ebf2c65926224c84cef75f68dad1d828fd2
HugoAhoy <zhjd3302@outlook.com>
2021-04-03 22:19:08 +0800

    use reindex normal kernel and group conv output, solve the problem of weight assign!

--- ThiNet/test.py
+++ ThiNet/test.py
@@ -14,22 +14,29 @@ def test_get_layers():
 
 def test_weight_assign():
     inch, outch = 3,2
-    kernel_weight = torch.arange(outch*inch*3*3,dtype=torch.float32).reshape(outch,inch,3,3)
-    # kernel_weight = torch.randn(outch, inch,3,3)
-    input = torch.arange(inch*5*5,dtype=torch.float32).reshape(1,inch,5,5)
-    # input = torch.randn(1,inch,5,5)
+    # kernel_weight = torch.arange(outch*inch*3*3,dtype=torch.float32).reshape(outch,inch,3,3)
+    kernel_weight = torch.randn(outch, inch,3,3)
+    # input = torch.arange(inch*5*5,dtype=torch.float32).reshape(1,inch,5,5)
+    input = torch.randn(1,inch,5,5)
+    # one = torch.ones(1,1,5,5)
+    # input = torch.cat([one*i for i in range(1,1+inch)],1)
     print(input)
     conv1 = nn.Conv2d(inch,outch, 3,padding=1,bias=False)
     conv2 = nn.Conv2d(inch,outch*inch, 3,groups = inch,padding=1,bias=False)
     conv1.weight.data = kernel_weight
-    conv2.weight.data = kernel_weight.reshape(inch*outch,1,3,3)
+    group_kernel_index = []
+    for i in range(inch):
+        group_kernel_index.extend([i+j*inch for j in range(outch)])
     
-    input = input
-    conv1 = conv1
-    conv2 = conv2
+    group_out_reindex = []
+    for i in range(outch):
+        group_out_reindex.extend([i+j*outch for j in range(inch)])
+    conv2.weight.data = kernel_weight.reshape(inch*outch,1,3,3)[group_kernel_index]
+    print(conv2.weight.data)
 
     out1 = conv1(input)
     out2 = conv2(input)
+    out2 = out2[:,group_out_reindex,...]
 
     print("out1",out1)
     print("out2",out2)
