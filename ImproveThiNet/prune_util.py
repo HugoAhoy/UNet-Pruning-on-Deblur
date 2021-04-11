738ec32b0339b2233bc757ed5c3ef88a61c969b4
HugoAhoy <zhjd3302@outlook.com>
2021-04-11 13:41:17 +0800

    add a Arbitary UNet that generate structure from a filternum dict

--- ImproveThiNet/prune_util.py
+++ ImproveThiNet/prune_util.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+from collections import namedtuple
 
 def get_filter_num_by_layer(model):
     filter_num = []
@@ -44,3 +45,50 @@ if __name__ == "__main__":
     print(pre)
     print(filter_num)
     print(len(suc), len(pre), len(filter_num))
+
+'''
+Return the setting of inconv, down, up, outconv
+all stuff are in namedtuple
+'''
+
+def parse_filternum_dict(filternum_dict)
+    stage = ((len(filternum_dict) -1) //2-1)//2
+    succeeding_strategy = unet_succeeding_strategy(stage)
+    preceding_strategy = get_preceding_from_succeding(preceding_strategy)
+
+    DoubleConvSetting = namedtuple('DoubleConvSetting', ['inch','out', 'mid'])
+    ConvSetting = namedtuple('ConvSetting',['inch','out'])
+    settingdict = {}
+
+    inc_in = 3
+    inc_mid = filternum_dict[0]
+    inc_out = filternum_dict[1]
+    settingdict['inc'] = DoubleConvSetting(inc_in, inc_out, inc_mid)
+    for i in range(stage):
+        inch = 0
+        for j in preceding_strategy[2+i*2]:
+            inch += filternum_dict[j]
+
+        mid = 0
+        for j in preceding_strategy[2+i*2]:
+            mid += filternum_dict[j]
+
+        out = filternum_dict[2+i*2+1]
+        settingdict['down{}'.format(i)] = DoubleConvSetting(inch, mid, out)
+
+    for i in range(stage, stage*2):
+        inch = 0
+        for j in preceding_strategy[2+i*2]:
+            inch += filternum_dict[j]
+
+        mid = 0
+        for j in preceding_strategy[2+i*2]:
+            mid += filternum_dict[j]
+
+        out = filternum_dict[2+i*2+1]
+        settingdict['up{}'.format(i-stage)] = DoubleConvSetting(inch, mid, out)
+    
+    outconv_in = filternum_dict(len(filternum_dict)-1)
+    settingdict['out'] = ConvSetting(outconv_in, 3)
+
+    return settingdict
